cba0c55565ba1ff1042f82ec19eaf99bf3a2ab80
Faisal Maqbool <lhe-faisalm@corp.expeditors.com>
2019-07-12 10:11:47 +0500

    Display manipulation

--- js/start.js
+++ js/start.js
@@ -1,1 +1,7 @@
-alert("Helloww World");
+document.getElementById("button").onclick = function() {
+
+    document.getElementById("confirm").innerHTML = "order placed. Check email for confirmation";
+
+    document.getElementById("button").style.display = "none";
+
+}
